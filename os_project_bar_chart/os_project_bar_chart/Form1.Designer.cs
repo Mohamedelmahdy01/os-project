1c98b701e63bcdf00cbc1062ccd9b935c8343240
mohamed mahmoud elmahdy <113891389+Mohamedelmahdy01@users.noreply.github.com>
2022-12-03 16:22:21 +0200

    Add files via upload

---
+++ os_project_bar_chart/os_project_bar_chart/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace os_project_bar_chart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.lblanimals = new System.Windows.Forms.Label();
+            this.mytimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(1, 36);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            series1.ChartArea = "ChartArea1";
+            series1.LabelForeColor = System.Drawing.Color.White;
+            series1.Legend = "Legend1";
+            series1.Name = "Cat";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Dog";
+            series2.YValuesPerPoint = 2;
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Bird";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Monkey";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(749, 417);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "Animals";
+            // 
+            // lblanimals
+            // 
+            this.lblanimals.AutoSize = true;
+            this.lblanimals.Location = new System.Drawing.Point(282, 18);
+            this.lblanimals.Name = "lblanimals";
+            this.lblanimals.Size = new System.Drawing.Size(117, 31);
+            this.lblanimals.TabIndex = 1;
+            this.lblanimals.Text = "Animals";
+            // 
+            // mytimer
+            // 
+            this.mytimer.Enabled = true;
+            this.mytimer.Interval = 1000;
+            this.mytimer.Tick += new System.EventHandler(this.mytimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(752, 584);
+            this.Controls.Add(this.lblanimals);
+            this.Controls.Add(this.chart1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label lblanimals;
+        private System.Windows.Forms.Timer mytimer;
+    }
+}
+
---
+++ os_project_bar_chart/os_project_bar_chart/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace os_project_bar_chart
+{
+    public partial class Form1 : Form
+    {
+        private int cat ;
+        private int dog ;
+        private int bird ;
+        private int monkey ;
+        private int day ;
+        private int x;
+        private int y;
+        private int z;
+        private int c;
+        private int cahrtmix;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+  
+        private void btnfill_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cat = 1;
+            dog = 1;
+            bird = 1;
+            monkey = 1;
+            day = 0;
+            cahrtmix = 0;
+            
+        
+        }
+
+        private void mytimer_Tick(object sender, EventArgs e)
+        {
+        
+            Random ran = new Random();
+            x = ran.Next(1, 6);
+            y = ran.Next(1, 6);
+            z = ran.Next(1, 6);
+            c = ran.Next(1, 6);
+
+
+            cat += x;
+            dog += y;
+            bird += z;
+            monkey += c;
+            day += 1;
+   
+            this.chart1.Series["Cat"].Points.AddXY(day, cat);
+            this.chart1.Series["Cat"].Points.AddXY(day, cat);
+            this.chart1.Series["Dog"].Points.AddXY(day, dog);
+            this.chart1.Series["Bird"].Points.AddXY(day, bird);
+            this.chart1.Series["Monkey"].Points.AddXY(day, monkey);
+            Invalidate();
+
+ 
+        }
+
+   
+    }
+}
